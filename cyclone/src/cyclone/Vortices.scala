412e2c8b2c0ecb85f9691515b29ea38017f25fab
Victor Hugo Borja <vborja@apache.org>
2020-10-17 21:03:20 -0500

    Fix cyclic dependencies between streams

--- cyclone/src/cyclone/Vortices.scala
+++ cyclone/src/cyclone/Vortices.scala
@@ -7,17 +7,17 @@ trait Waterspout[I, S, O] extends Vortex[I, S, O] {
   protected def makeInputHandler(updates: EventStream[InputHandler]): Signal[InputHandler]
   protected def makeActionHandler(updates: EventStream[ActionHandler]): Signal[ActionHandler]
 
-  override val state: Signal[S]                               = makeState(stateStream)
-  override protected val inputHandler: Signal[InputHandler]   = makeInputHandler(inputHandlerStream)
-  override protected val actionHandler: Signal[ActionHandler] = makeActionHandler(actionHandlerStream)
+  override lazy val state: Signal[S]                               = makeState(stateStream)
+  override protected lazy val inputHandler: Signal[InputHandler]   = makeInputHandler(inputHandlerStream)
+  override protected lazy val actionHandler: Signal[ActionHandler] = makeActionHandler(actionHandlerStream)
 }
 
 trait Landspout[I, S, O] extends Vortex[I, S, O] {
   protected val initialState: State
-  protected val initialInputHandler: InputHandler   = { case _ => noEffect }
-  protected val initialActionHandler: ActionHandler = { case _ => noEffect }
+  protected lazy val initialInputHandler: InputHandler   = { case _ => noEffect }
+  protected lazy val initialActionHandler: ActionHandler = { case _ => noEffect }
 
-  override val state: Signal[S]                               = stateStream.startWith(initialState)
-  override protected val inputHandler: Signal[InputHandler]   = inputHandlerStream.startWith(initialInputHandler)
-  override protected val actionHandler: Signal[ActionHandler] = actionHandlerStream.startWith(initialActionHandler)
+  override lazy val state: Signal[S]                               = stateStream.startWith(initialState)
+  override protected lazy val inputHandler: Signal[InputHandler]   = inputHandlerStream.startWith(initialInputHandler)
+  override protected lazy val actionHandler: Signal[ActionHandler] = actionHandlerStream.startWith(initialActionHandler)
 }
