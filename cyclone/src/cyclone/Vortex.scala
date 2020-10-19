3e30f16c242963fa7d77a4b2c2bc56fc6bf6c49f
Victor Borja <vborja@apache.org>
2020-10-19 13:23:15 -0500

    Implement between combinator (#7)

--- cyclone/src/cyclone/Vortex.scala
+++ cyclone/src/cyclone/Vortex.scala
@@ -10,7 +10,7 @@ trait Vortex[E <: Element, I, S, O] extends FlowTypes[E, I, S, O] {
   def bind(initialFlow: Flow[_] = EmptyFlow): Binder[El]
 }
 
-object Vortex {
+object Vortex extends Cyclone with Between {
   implicit def toWriteBus[I](cyclone: Vortex[_, I, _, _]): WriteBus[I] =
     cyclone.input
 
@@ -19,53 +19,4 @@ object Vortex {
 
   implicit def toEventStream[O](cyclone: Vortex[_, _, _, O]): EventStream[O] =
     cyclone.output
-
-  case class Cycle[E <: Element, I, S, O] private[Vortex] () extends Flows[E, I, S, O] with Implicits {
-
-    def apply(
-        initState: S,
-        initFlow: Flow[_] = emptyFlow
-    )(inHandler: Handler = emptyHandler): Vortex[E, I, S, O] =
-      new Landspout[E, I, S, O] {
-        override protected lazy val initialState: State     = initState
-        override protected lazy val initialHandler: Handler = inHandler
-        override protected val initialFlow: Flow[_]         = initFlow
-      }
-
-  }
-
-  case class Apply[E <: Element, I, S, O] private[Vortex] () {
-    def build(fn: Cycle[E, I, S, O] => Vortex[E, I, S, O]): Vortex[E, I, S, O] =
-      fn(Cycle[E, I, S, O]())
-  }
-
-  def apply[E <: Element, I, S, O]: Apply[E, I, S, O] = Apply()
-//  final class Between private[Cyclone] (left: Cyclone[_, _, _, _], right: Cyclone[_, _, _, _]) {
-//    class Apply(flows: Flows[_, _, _, _]) {
-//      def apply(initState: flows.State, initHandler: flows.Handler): Cyclone[E, I, S, O] =
-//        new Landspout {
-//          override val flow = flows
-//          import flow._
-//          override protected val initialState: State     = initState
-//          override protected val initialHandler: Handler = initHandler
-//          override def bind(initialFlow: Flow[_]): Binder[El] = {
-//            ReactiveElement.bindCallback[El](_) { ctx =>
-//              ctx.thisNode.amend(
-//                super.bind(initialFlow),
-//                left.output.map(Left(_)) --> input,
-//                right.output.map(Right(_)) --> input,
-//                output.collect { case Left(v)  => v } --> left.input,
-//                output.collect { case Right(v) => v } --> right.input
-//              )
-//            }
-//          }
-//        }
-//    }
-//
-//    def apply[E <: Element, S]: Apply =
-//      new Apply(
-//        new Flows[E, Either[left.flow.Input, right.flow.Input], S, Either[left.flow.Output, right.flow.Output]] {}
-//      )
-//  }
-
 }
