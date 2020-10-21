634ed20d3df98b440e4aacf42b63d6f72df85d10
Victor Hugo Borja <vborja@apache.org>
2020-10-20 19:43:23 -0500

    Add event handler flows

--- cyclone/src/cyclone/Flows.scala
+++ cyclone/src/cyclone/Flows.scala
@@ -1,7 +1,11 @@
 package cyclone
 
 import com.raquo.airstream.eventbus.EventBus
+import com.raquo.domtypes.generic.builders.Tag
 import com.raquo.laminar.api.L._
+import com.raquo.laminar.emitter.EventPropTransformation
+import com.raquo.laminar.nodes.ReactiveElement
+import org.scalajs.dom
 
 trait Flows[E <: Element, I, S, O] extends FlowTypes[E, I, S, O] {
 
@@ -52,7 +56,7 @@ trait Flows[E <: Element, I, S, O] extends FlowTypes[E, I, S, O] {
   def fromFlowStream[X](fn: => EventStream[Flow[X]]): Flow[X] =
     FromStream[X](() => fn)
 
-  def callback[X](cb: => ((X => Unit) => Unit)): Flow[X] =
+  def fromCallback[X](cb: => ((X => Unit) => Unit)): Flow[X] =
     FromStream[X] { () =>
       val bus = new EventBus[X]
       cb(bus.writer.onNext)
@@ -64,6 +68,68 @@ trait Flows[E <: Element, I, S, O] extends FlowTypes[E, I, S, O] {
 
   def element: Flow[E] =
     context.map(_.thisNode)
+
+  private def asReactiveElement[Ref <: dom.Element](el: Ref) = {
+    new ReactiveElement[Ref] {
+      override val tag: Tag[ReactiveElement[Ref]] = new Tag(el.tagName, false)
+      override val ref: Ref                       = el
+    }
+  }
+
+  def elementById[Ref <: dom.Element](id: String): Flow[ReactiveElement[Ref]] =
+    pure {
+      val el = dom.document.getElementById(id)
+      asReactiveElement[Ref](el.asInstanceOf[Ref])
+    }
+
+  def elementBySelector[Ref <: dom.Element](selector: String): Flow[ReactiveElement[Ref]] =
+    pure {
+      val el = dom.document.querySelector(selector)
+      asReactiveElement[Ref](el.asInstanceOf[Ref])
+    }
+
+  def elementChildBySelector[Ref <: dom.Element](selector: String): Flow[ReactiveElement[Ref]] =
+    element.map { parent =>
+      val el = parent.ref.querySelector(selector)
+      asReactiveElement[Ref](el.asInstanceOf[Ref])
+    }
+
+  type EventInContext[EL <: Element, X, Y] = EL => EventStream[X] => EventStream[Y]
+  object EventInContext {
+    def identity[El <: Element, X]: EventInContext[El, X, X] = { el: El => ev => ev }
+  }
+
+  def events[Ev <: dom.Event, V](t: EventPropTransformation[Ev, V]): Flow[EventStream[V]] =
+    eventsOf(element, t, EventInContext.identity[E, V])
+
+  def events[Ev <: dom.Event, V, R](
+      t: EventPropTransformation[Ev, V],
+      inContext: EventInContext[E, V, R]
+  ): Flow[EventStream[R]] =
+    eventsOf(element, t, inContext)
+
+  def eventsOf[EL <: Element, Ev <: dom.Event, V, R](
+      element: Flow[EL],
+      t: EventPropTransformation[Ev, V],
+      inContext: EventInContext[EL, V, R]
+  ): Flow[EventStream[R]] =
+    element.map(e => e.events(t).compose(inContext(e)))
+
+  def fromEvents[Ev <: dom.Event, V](t: EventPropTransformation[Ev, V]): Flow[V] =
+    eventsOf(element, t, EventInContext.identity[E, V]).flatMap(fromStream(_))
+
+  def fromEvents[Ev <: dom.Event, V, R](
+      t: EventPropTransformation[Ev, V],
+      inContext: EventInContext[E, V, R]
+  ): Flow[R] =
+    eventsOf(element, t, inContext).flatMap(fromStream(_))
+
+  def fromEventsOf[EL <: Element, Ev <: dom.Event, V, R](
+      element: Flow[EL],
+      t: EventPropTransformation[Ev, V],
+      inContext: EventInContext[EL, V, R]
+  ): Flow[R] =
+    eventsOf(element, t, inContext).flatMap(fromStream(_))
 
   def bind(binder: => Binder[E]): Flow[E] =
     element.map(_.amend(binder))
